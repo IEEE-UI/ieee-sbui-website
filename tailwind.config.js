9fea3fad130eabe18bfdbffde7656e8ded909a93
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-05-22 13:31:38 +0700

    add memberRegistration and update package config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html', './assets/js/*.js', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ['./*.html', './assets/js/*.js', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
